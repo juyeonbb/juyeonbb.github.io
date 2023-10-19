4946aa1f5f3107e9aa28932f35ea991d0e135f88
juyeonbb <januielee@kbrainc.com>
2023-10-19 17:55:32 +0900

    * 표준협회 사용자 진행중

--- preojects/ksaedu-user/pagelist/data/data.js
+++ preojects/ksaedu-user/pagelist/data/data.js
@@ -8,43 +8,49 @@ data.common = {
       url: 'layout',
       page: 'layout'
     },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'guide/color',
-    //   page: 'colors'
-    // },
-    // {
-    //   stateClass: 'edited',
-    //   url: 'guide/fonts',
-    //   page: 'fonts'
-    // },
+    {
+      stateClass: 'complete',
+      url: 'guide/color',
+      page: 'colors'
+    },
+    {
+      stateClass: 'complete',
+      url: 'guide/fonts',
+      page: 'fonts'
+    },
     // {
     //   stateClass: 'waiting',
     //   url: 'guide/popup',
     //   page: 'popup'
     // },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'guide/form',
-    //   page: 'form'
-    // },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'guide/buttons',
-    //   page: 'buttons'
-    // },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'guide/tab',
-    //   page: 'tab'
-    // },
-    // {
-    //   stateClass: 'complete',
-    //   url: 'guide/table',
-    //   page: 'table'
-    // },
+    {
+      stateClass: 'complete',
+      url: 'guide/form',
+      page: 'form'
+    },
+    {
+      stateClass: 'complete',
+      url: 'guide/buttons',
+      page: 'buttons'
+    },
+    {
+      stateClass: 'complete',
+      url: 'guide/tab',
+      page: 'tab'
+    },
+    {
+      stateClass: 'complete',
+      url: 'guide/table',
+      page: 'table'
+    },
+    {
+      stateClass: 'complete',
+      url: 'guide/icons',
+      page: 'icons'
+    },
   ]
 }
+
 data.main = {
   title: '메인',
   list: [
@@ -52,6 +58,94 @@ data.main = {
       stateClass: 'working',
       url: 'main',
       page: 'main'
+    },
+  ]
+}
+
+data.login = {
+  title: '로그인',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '01_로그인',
+      page: '로그인'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_휴면계정안내',
+      page: '휴면계정안내'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_아이디찾기_본인인증',
+      page: '아이디 찾기 > 본인인증'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_아이디찾기_아이디찾기결과',
+      page: '아이디 찾기 > 결과'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_비밀번호찾기_비밀번호변경하기',
+      page: '비밀번호 찾기 > 비밀번호 변경하기'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_SNS간편로그인_연동',
+      page: 'SNS 간편 로그인 > 연동'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_SNS간편로그인_연동조회',
+      page: 'SNS 간편 로그인 > 연동 > 조회'
+    },
+  ]
+}
+
+data.signIn = {
+  title: '회원가입',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '01_회원가입_약관동의',
+      page: '회원가입 > 약관동의'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_회원가입_본인인증',
+      page: '회원가입 > 본인인증'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_회원가입_회원정보입력',
+      page: '회원가입 > 회원정보 입력'
+    },
+    {
+      stateClass: 'complete',
+      url: '01_회원가입_가입완료',
+      page: '회원가입 > 가입완료'
+    },
+  ]
+}
+
+data.notice = {
+  title: '고객지원',
+  list: [
+    {
+      stateClass: 'working',
+      url: '07_고객지원_공지사항',
+      page: '공지사항'
+    },
+    {
+      stateClass: 'working',
+      url: '07_고객지원_공지사항_상세',
+      page: '공지사항 > 상세'
+    },
+    {
+      stateClass: 'working',
+      url: '07_고객지원_FAQ',
+      page: '공지사항 > FAQ'
     },
   ]
 }
