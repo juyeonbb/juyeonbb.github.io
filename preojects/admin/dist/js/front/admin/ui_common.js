83fc03e05a4c3b0fb130450bf378ae3b424b77a0
juyeonbb <januielee@kbrainc.com>
2024-04-04 13:23:29 +0900

    * 관리자 페이지

--- preojects/admin/dist/js/front/admin/ui_common.js
+++ preojects/admin/dist/js/front/admin/ui_common.js
@@ -97,7 +97,6 @@ $('.popup .popup-content').each(function () {
  * ******************************************************************
  * */
 
-
 /**
  * Count txt
  * */
@@ -110,3 +109,53 @@ function countChar(val,target) {
 /**
  * ******************************************************************
  * */
+
+let lnbUI = {
+  click : function (target, speed) {
+    let _self = this,
+        $target = $(target);
+        _self.speed = speed || 300;
+
+    $target.each(function(){
+      if(findChildren($(this))) {
+        return;
+      }
+      $(this).addClass('no-depth');
+    });
+
+    function findChildren(obj) {
+      return obj.find('> ul').length > 0;
+    }
+
+    $target.on('click','a', function(e){
+      e.stopPropagation();
+      let $this = $(this),
+          $depthTarget = $this.next(),
+          $siblings = $this.parent().siblings();
+
+      $this.parent('li').find('ul li').removeClass('on');
+      $siblings.removeClass('on');
+      $siblings.find('ul').slideUp(250);
+
+      if($depthTarget.css('display') == 'none') {
+        _self.activeOn($this);
+        $depthTarget.slideDown(_self.speed);
+      } else {
+        $depthTarget.slideUp(_self.speed);
+        _self.activeOff($this);
+      }
+
+    })
+
+  },
+  activeOff : function($target) {
+    $target.parent().removeClass('on');
+  },
+  activeOn : function($target) {
+    $target.parent().addClass('on');
+  }
+};
+
+$(function(){
+  lnbUI.click('._mainMenu', 300)
+});
