2aa24851bad11d67cd8f772bc2eb3de56663bb2b
juyeonbb <januielee@kbrainc.com>
2026-01-27 15:30:23 +0900

    pvcp update

---
+++ projects/pvcp/pagelist/data/data.js
@@ -0,0 +1,152 @@
+const data = {}
+
+data.guide = {
+    title: '가이드',
+    list: [
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/accordion',
+        //     page: 'accordion'
+        // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/badge',
+        //     page: 'badges'
+        // },
+        {
+            stateClass: 'complete',
+            url: 'guide/breadcrumb',
+            page: 'breadcrumb'
+        },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/buttons',
+        //     page: 'buttons'
+        // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/form',
+        //     page: 'form'
+        // },
+        {
+            stateClass: 'complete',
+            url: 'guide/pagination',
+            page: 'pagination'
+        },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/popup',
+        //     page: 'popup'
+        // // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/table',
+        //     page: 'table'
+        // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/tabs',
+        //     page: 'tabs'
+        // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/tooltip',
+        //     page: 'tooltip'
+        // },
+        // {
+        //     stateClass: 'complete',
+        //     url: 'guide/etc',
+        //     page: 'etc'
+        // },
+        {
+            stateClass: 'complete',
+            url: 'guide/list',
+            page: 'list'
+        },
+    ]
+}
+
+data.layout = {
+    title: '레이아웃',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'guide/00_layout',
+            page: '메인 레이아웃'
+        },
+        {
+            stateClass: 'complete',
+            url: 'guide/00_sub_layout',
+            page: '서브 레이아웃 (2depth)'
+        },
+        {
+            stateClass: 'complete',
+            url: 'guide/00_sub_layout2',
+            page: '서브 레이아웃 (3depth)'
+        },
+    ]
+}
+
+data.common = {
+    title: '공통 페이지',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'com/00_서비스점검페이지',
+            page: 'TEST'
+        },
+    ]
+}
+
+data.main = {
+    title: '메인',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'main/메인',
+            page: '메인'
+        },
+    ]
+}
+
+data.search = {
+    title: '통합검색',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'main/통합검색_인물',
+            page: '통합검색 > 인물'
+        },
+        {
+            stateClass: 'complete',
+            url: 'main/통합검색_역사',
+            page: '통합검색 > 역사'
+        },
+        {
+            stateClass: 'complete',
+            url: 'main/통합검색_현충시설',
+            page: '통합검색 > 현충시설'
+        },
+        {
+            stateClass: 'complete',
+            url: 'main/통합검색',
+            page: '통합검색'
+        },
+    ]
+}
+
+data.archive = {
+    title: '보훈자료',
+    list: [
+        {
+            stateClass: 'complete',
+            url: 'pvData/보훈자료',
+            page: '보훈자료'
+        },
+        {
+            stateClass: 'complete',
+            url: 'pvData/보훈자료_인물',
+            page: '보훈자료 > 인물'
+        },
+    ]
+}
