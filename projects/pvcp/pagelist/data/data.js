44de9ba03a60c708add773daca87012298b0331b
juyeonbb <januielee@kbrainc.com>
2026-02-05 15:27:44 +0900

    pvcp update

--- projects/pvcp/pagelist/data/data.js
+++ projects/pvcp/pagelist/data/data.js
@@ -219,12 +219,12 @@ data.archive = {
             page: '보훈자료 > 지도검색'
         },
         {
-            stateClass: 'working',
+            stateClass: 'complete',
             url: 'pvCntnts/보훈자료_지도검색_검색결과',
             page: '보훈자료 > 지도검색 (검색결과 팝업)'
         },
         {
-            stateClass: 'working',
+            stateClass: 'complete',
             url: 'pvCntnts/보훈자료_지도검색_검색결과2',
             page: '보훈자료 > 지도검색 (검색결과 팝업)'
         },
